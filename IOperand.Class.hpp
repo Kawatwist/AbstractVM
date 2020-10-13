c9673df49be20a853e363a7ef4846c1f912c20fb
Kawatwist <masse.loic2@hotmail.com>
2020-10-13 21:18:38 +0200

    Operand Interfase

---
+++ IOperand.Class.hpp
@@ -0,0 +1,22 @@
+#ifndef IOPERAND_HPP
+# define IOPERAND_HPP
+
+#include <string>
+#include <iostream>
+
+class IOperand {
+    public:
+        virtual int             getPrecision(void) const= 0;
+        virtual eOperandType    getType(void) const= 0;
+        
+        virtual IOperand const *operator+( IOperand const& rhs ) const= 0;
+        virtual IOperand const *operator-( IOperand const& rhs ) const= 0;
+        virtual IOperand const *operator*( IOperand const& rhs ) const= 0;
+        virtual IOperand const *operator/( IOperand const& rhs ) const= 0;
+        virtual IOperand const *operator%( IOperand const& rhs )const= 0;
+        
+        virtual std::string const & toString(void) const= 0;
+        
+        ~IOperand(void) {}
+};
+#endif
